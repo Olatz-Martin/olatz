c2ddcfb9535ad5f3a216760b4d2662258a86faf4
usuario <usuario@192.168.0.20>
2018-05-02 21:38:22 +0200

    commit resuelve el conflicto

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,9 +1,10 @@
 package prueba;
 
 public class Prueba {
-
+String Hola;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	// TODO Auto-generated method stub
 
 	}
 
